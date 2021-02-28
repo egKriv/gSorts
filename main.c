45f96147de10e26ef870bb9cbd9e629bce67a40a
egKriv <ekrivotulov@gmail.com>
2021-02-28 17:32:39 +0300

    generator of unsorted double array

--- main.c
+++ main.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <malloc.h>
+#include <stdlib.h>
 
-
-int cmps(double a, double b);
-void swap(double* a, double* b);
-
-
+int cmps(double a, double b); // compares a and b. return -1 if b should be before a, 1 if a before b, 0 if no matterr
+void swap(double* a, double* b); // swap data in a and b
+double* gen_arr(int n, int type); // generate array of n doubles. type defines order: 1 - sorted, 2 - reverse sorted, 3 - unsorted
+void print_arr(int n, double* a); // prints array of n doubles
+double fRand(void); // generate random double
 int comparison_count = 0;
 int swap_count = 0;
 
 int main (void)
 {
-    FILE *fp;
-    int i;
-    double a, b;
-    char name[] = "cmpExamples.txt";
-    if ((fp = fopen(name, "r")) == NULL)
-    {
-        printf("Не удалось открыть файл");
-        getchar();
-        return 0;
-    }
-    for(i = 0; i< 5; i++)
-    {
-        fscanf(fp, "%lf", &a);
-        fscanf(fp, "%lf", &b);
-
-        printf("%d\n", cmps(a, b));
-    }
-    fclose(fp);
+    int n = 10;
+    srand(2);
+    double *a = gen_arr(n, 3);
+    print_arr(n, a);
+    free(a);
     return 0;
 }
 
 
 int cmps(double a, double b)
 {
-    //note each comparison
     comparison_count++;
-    //return if |a|>|b| or |a|=|b| or |a|<|b|.
     if (abs(a) > abs(b))
         return 1;
     if (abs(a) == abs(b))
@@ -52,4 +40,53 @@ void swap(double *a, double *b)
     a[0] = b[0];
     b[0] = x;
     return;
+}
+
+double* gen_arr(int n, int type)
+{
+    int i;
+    double* a = (double*)malloc(n*sizeof(double));
+    for (i = 0; i < n; i++) // generate array
+    {
+        a[i] = fRand();
+    }
+    //sort if needed
+    if(type == 3)
+    {
+        return a;
+    }
+    // here should be two ways more
+    free(a); // just in case
+    return NULL;
+}
+void print_arr(int n, double* a)
+{
+    int i;
+    for (i=0; i<n; i++)
+    {
+        printf("%e ", a[i]); // exponential output is more comfortable
+    }
+    printf("\n");
+}
+/*void bubblesort(double* a, int m)
+{
+    int i=0, j=m-1;
+    for (; j>0; j--)
+    {
+        for(i=0; i<j; i++)
+        {
+            if(cmp(a[i], a[i+1])<0)
+            {
+                swap(a[i], a[i+1]);
+            }
+        }
+    }
+}*/
+double fRand(void)
+{
+    //create 8 random bytes of double
+    char c[8] = {rand()%256, rand()%256, rand()%256, rand()%256,
+                rand()%256, rand()%256, rand()%256, rand()%256};
+    double *f = (double*)c;
+    return *f;
 }
